958f636edb5073d846862d7df4e023cf1f698646
david223-ssp <66477612+david223-ssp@users.noreply.github.com>
2024-01-22 23:30:24 +0000

    novo mapa

---
+++ resources/[addons]/[maps]/molo_hellsangel/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+this_is_a_map "yes"
+
+data_file('DLC_ITYP_REQUEST')('stream/garage_hells.ytyp')
+files {
+
+"interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+dependency '/assetpacks'
