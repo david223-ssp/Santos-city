ff62d3ed4a2e2b6cfbf58461dd00b342aea1cdbe
david223-ssp <66477612+david223-ssp@users.noreply.github.com>
2024-01-22 01:02:38 +0000

    cfx-santos

---
+++ resources/[addons]/[maps]/cfx-santos/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map "yes"
+
+files {
+	"stream/*",
+	"stream/*/**",
+	"stream/*/*/**",
+	"stream/*/*/*/**",
+	"interiorproxies.meta"
+}
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+data_file "DLC_ITYP_REQUEST" "stream/ForestNorth/forest_n_slod.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/ForestSouth/forest_s_slod.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "stream/Sprays/sprays.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Badges/v_badges.ytyp"
