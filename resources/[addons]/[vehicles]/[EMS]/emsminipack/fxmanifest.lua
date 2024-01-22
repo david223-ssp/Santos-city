ad24b2e139440df7785c80923a89261993759973
david223-ssp <66477612+david223-ssp@users.noreply.github.com>
2024-01-22 00:54:25 +0000

    vehicles

---
+++ resources/[addons]/[vehicles]/[EMS]/emsminipack/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'meta/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'meta/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'meta/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'meta/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'meta/**/carvariations.meta'
+
+files {
+  'meta/**/vehiclelayouts.meta',
+  'meta/**/handling.meta',
+  'meta/**/vehicles.meta',
+  'meta/**/carcols.meta',
+  'meta/**/carvariations.meta',
+}
