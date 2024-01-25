5d572433fbf27fc8405b911d568fb66ad42219a0
david223-ssp <66477612+david223-ssp@users.noreply.github.com>
2024-01-25 20:19:51 +0000

    reformolaçao

--- resources/[qb]/[jobs]/qb-pawn/config.lua
+++ resources/[qb]/[jobs]/qb-pawn/config.lua
@@ -1,1 +1,14 @@
 Config = {}
+
+Config.PawnLocation = {
+    [1] = {
+            coords = vector3(168.1, -1311.13, 29.34),
+            length = 1.5,
+            width = 1.8,
+            heading = 207.0,
+            debugPoly = false,
+            minZ = 28.0,
+            maxZ = 31.0,
+            distance = 2.0
+        },
+    }
