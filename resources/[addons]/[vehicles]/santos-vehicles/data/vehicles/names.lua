9e1ddfb139f6bedf0c44070f295c73a682c5b4cc
david223-ssp <66477612+david223-ssp@users.noreply.github.com>
2024-02-24 19:44:52 +0000

    vehicles

---
+++ resources/[addons]/[vehicles]/santos-vehicles/data/vehicles/names.lua
@@ -0,0 +1,4 @@
+Citizen.CreateThread(function()
+	AddTextEntry('0x3B17AC66', 'R1200 BMW Vlog')
+	AddTextEntry('0xF88A5863', 'bau GS R1200')
+end)
---
+++ resources/[addons]/[vehicles]/santos-vehicles/fxmanifest.lua
@@ -0,0 +1,229 @@
+fx_version "bodacious"
+game "gta5"
+
+client_scripts {
+	"data/vehicles/names.lua"
+}
+
+files {
+	"audio/services/*",
+	"audio/services/**/*",
+	"audio/services/**/**/*",
+	"audio/vehicles/*",
+	"audio/vehicles/**/*",
+	"audio/vehicles/**/**/*",
+	"data/armored/*",
+	"data/services/**/*",
+	"data/vehicles/*",
+	"stream/*",
+	"stream/**/*",
+	"stream/**/**/*"
+}
+-- Armored Data
+data_file "HANDLING_FILE" "data/armored/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/armored/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/armored/carvariations.meta"
+
+-- Services Data
+data_file "HANDLING_FILE" "data/services/ambulance2/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/ambulance2/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/ambulance2/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/ambulance2/carvariations.meta"
+
+data_file "VEHICLE_VARIATION_FILE" "data/services/maverick2/carvariations.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/maverick2/vehicles.meta"
+
+data_file "CARCOLS_FILE" "data/services/B412/carcols.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/B412/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/B412/carvariations.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/services/B412/vehiclelayouts.meta"
+
+data_file "HANDLING_FILE" "data/services/bcat/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/bcat/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/bcat/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/bcat/carvariations.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/services/bcat/vehiclelayouts.meta"
+
+data_file "HANDLING_FILE" "data/services/bmwm3/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/bmwm3/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/bmwm3/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/bmwm3/carvariations.meta"
+
+data_file "HANDLING_FILE" "data/services/bmwm4/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/bmwm4/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/bmwm4/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/bmwm4/carvariations.meta"
+
+data_file "HANDLING_FILE" "data/services/bmwm5/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/bmwm5/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/bmwm5/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/bmwm5/carvariations.meta"
+
+data_file "HANDLING_FILE" "data/services/e63/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/e63/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/e63/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/e63/carvariations.meta"
+
+data_file "HANDLING_FILE" "data/services/fpace/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/fpace/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/fpace/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/fpace/carvariations.meta"
+
+data_file "HANDLING_FILE" "data/services/jettagli/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/jettagli/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/jettagli/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/jettagli/carvariations.meta"
+
+data_file "HANDLING_FILE" "data/services/tiger/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/tiger/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/tiger/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/tiger/carvariations.meta"
+
+data_file "HANDLING_FILE" "data/services/tundra/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/services/tundra/vehicles.meta"
+data_file "CARCOLS_FILE" "data/services/tundra/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/services/tundra/carvariations.meta"
+
+-- Vehicles Data
+data_file "CARCOLS_FILE" "data/vehicles/carcols.meta"
+data_file "HANDLING_FILE" "data/vehicles/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/vehicles/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/vehicles/carvariations.meta"
+data_file "CARCOLS_GEN9_FILE" "data/vehicles/carcols_gen9.meta"
+data_file "CARMODCOLS_GEN9_FILE" "data/vehicles/carmodcols_gen9.meta"
+
+-- Services Audio
+data_file "AUDIO_SYNTHDATA" "audio/services/bmwm3/s55b30_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/services/bmwm3/s55b30_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/services/bmwm3/s55b30_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/services/bmwm3/dlc_s55b30"
+
+data_file "AUDIO_SYNTHDATA" "audio/services/bmwm5/s63b44_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/services/bmwm5/s63b44_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/services/bmwm5/s63b44_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/services/bmwm5/dlc_s63b44"
+
+data_file "AUDIO_SYNTHDATA" "audio/services/e63/mbnzc63eng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/services/e63/mbnzc63eng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/services/e63/mbnzc63eng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/services/e63/dlc_mbnzc63eng"
+
+data_file "AUDIO_SYNTHDATA" "audio/services/jettagli/ea888_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/services/jettagli/ea888_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/services/jettagli/ea888_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/services/jettagli/dlc_ea888"
+
+data_file "AUDIO_SYNTHDATA" "audio/services/tundra/chevydmaxeng_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/services/tundra/chevydmaxeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/services/tundra/chevydmaxeng_sounds.dat"
+data_file "AUDIO_WAVEPACK" "audio/services/tundra/dlc_chevydmaxeng"
+
+-- Vehicles Audio
+data_file "AUDIO_WAVEPACK" "audio/vehicles/lgcy04murciv12/dlc_lgcy04murciv12"
+data_file "AUDIO_SYNTHDATA" "audio/vehicles/lgcy04murciv12/lgcy04murciv12_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/lgcy04murciv12/lgcy04murciv12_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/lgcy04murciv12/lgcy04murciv12_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/s85b50/dlc_s85b50"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/s85b50/s85b50_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/s85b50/s85b50_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/ea888/dlc_ea888"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/ea888/ea888_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/ea888/ea888_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/aq2jzgterace/dlc_aq2jzgterace"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/aq2jzgterace/aq2jzgterace_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/aq2jzgterace/aq2jzgterace_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/sultanrsv8/dlc_v8sultanrs"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/sultanrsv8/sultanrsv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/sultanrsv8/sultanrsv8_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/cw2019/dlc_cw2019"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/cw2019/cw2019_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/cw2019/cw2019_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/s15/dlc_s15sound"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/s15/s15sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/s15/s15sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/n4g63t/dlc_n4g63t"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/n4g63t/n4g63t_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/n4g63t/n4g63t_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/elegyx/dlc_elegyx"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/elegyx/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/elegyx/elegyx_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/r34/dlc_r34sound"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/r34/r34sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/r34/r34sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/gt3rstun/dlc_gt3rstun"
+data_file "AUDIO_SYNTHDATA" "audio/vehicles/gt3rstun/gt3rstun_amp.dat"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/gt3rstun/gt3rstun_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/gt3rstun/gt3rstun_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/aq06nhonc30a/dlc_aq06nhonc30a"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/aq06nhonc30a/aq06nhonc30a_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/aq06nhonc30a/aq06nhonc30a_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/trumpetzr/dlc_trumpetzr"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/trumpetzr/trumpetzrc_game.dat"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/trumpetzr/trumpetzr_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/trumpetzr/trumpetzrc_sounds.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/trumpetzr/trumpetzr_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/f10m5/dlc_f10m5"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/f10m5/f10m5_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/f10m5/f10m5_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/s15sound/dlc_s15sound"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/s15sound/s15sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/s15sound/s15sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/sentinelsg4/dlc_sentinelsg4"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/sentinelsg4/sentinelsg4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/sentinelsg4/sentinelsg4_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/lfasound/dlc_lfasound"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/lfasound/lfasound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/lfasound/lfasound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/rb26dett/dlc_rb26dett"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/rb26dett/rb26dett_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/rb26dett/rb26dett_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/subaruej20/dlc_subaruej20"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/subaruej20/subaruej20_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/subaruej20/subaruej20_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/rotary7/dlc_rotary7"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/rotary7/rotary7_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/rotary7/rotary7_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/f20c/dlc_f20c"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/f20c/f20c_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/f20c/f20c_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/aqls7raceswap/dlc_aqls7raceswap"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/aqls7raceswap/aqls7raceswap_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/aqls7raceswap/aqls7raceswap_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/ftypesound/dlc_ftypesound"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/ftypesound/ftypesound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/ftypesound/ftypesound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/majimalm/dlc_majimagt"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/majimalm/majimalm_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/majimalm/majimalm_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/ta488f154/dlc_ta488f154"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/ta488f154/ta488f154_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/ta488f154/ta488f154_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/vehicles/cvpiv8/dlc_cvpiv8"
+data_file "AUDIO_GAMEDATA" "audio/vehicles/cvpiv8/cvpiv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/vehicles/cvpiv8/cvpiv8_sounds.dat"
